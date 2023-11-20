d6ad22a7a8ecaa6a56d693078141ee8c9a1e7e6c
abhinav swami <abhinav@tradelab.in>
2023-11-20 16:25:22 +0530

    moved DB credentials to env file removed from code

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const taskRoutes = require("./routes/taskRoutes");
+const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+const MONGODB_HOSTURL = process.env.MONGODB_HOSTURL;
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-mongoose.connect("mongodb+srv://abhinav:undefinednull@tlb.c2wnq3f.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect(`mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_HOSTURL}/?retryWrites=true&w=majority`);
 
 app.use(bodyParser.json());
 app.use("/api", taskRoutes);
